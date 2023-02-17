ffe9f2dcae49b4a9532b1dc64a6f78190ac018e3
fadi <fady.yosry2018@gmail.com>
2023-02-17 14:00:16 +0200

    return name of gym instead of pk

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from .models import Member, Gym
 
-class MemberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Member
-        fields = '__all__'
-
 class GYMSerializer(serializers.ModelSerializer):
     class Meta:
         model = Gym
         fields = '__all__'
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = '__all__'
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['gym'] = instance.gym.name
+        return data
+
