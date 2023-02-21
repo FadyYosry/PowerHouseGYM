be05af9714a60ae885da1465629d79bcd64403ba
fadi <fady.yosry2018@gmail.com>
2023-02-21 17:20:04 +0200

    Add Model of AI in Machine Learning dir

--- machineLearning/views.py
+++ machineLearning/views.py
@@ -1,8 +1,15 @@
 import cv2
+import os
 from django.http import StreamingHttpResponse
 
 def capture_frames(request):
     cap = cv2.VideoCapture(0)
+    # Get the path to the classifier file
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    classifier_path = os.path.join(BASE_DIR, 'classifiers/haarcascade_frontalface_default.xml')
+
+# Load the classifier
+    detector = cv2.CascadeClassifier(classifier_path)
 
     def generate():
         while True:
@@ -10,9 +17,17 @@ def capture_frames(request):
             if not ret:
                 break
 
-            # process the frame here
+            # detect faces in the frame
+            faces = detector.detectMultiScale(frame, 1.1, 7)
 
+            # draw a rectangle around each face
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            # convert the frame to JPEG format
             _, jpeg = cv2.imencode('.jpg', frame)
+
+            # yield the frame as a multipart/x-mixed-replace response
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
 
