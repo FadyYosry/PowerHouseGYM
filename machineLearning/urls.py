3f8f559239025f0a7f980404fd3c0c6c2a63d6cd
fadi <fady.yosry2018@gmail.com>
2023-05-20 00:20:48 +0300

    Add stream camera for url

--- machineLearning/urls.py
+++ machineLearning/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import capture_frames,face_detection, classifyPose ,classify_pose_real_time , stream_camera, pose_classifier, classify_pose_real_time2
 
 urlpatterns = [
-    path('', stream_camera, name='stream_camera'),
+    path('stream_camera/', stream_camera, name='stream_camera'),
     path('face_detection/', face_detection, name='face_detection'),
     path('classify_pose/', classifyPose, name='classify_pose'),
     path('classify_pose_real_time/', classify_pose_real_time, name='classify_pose_real_time'),
