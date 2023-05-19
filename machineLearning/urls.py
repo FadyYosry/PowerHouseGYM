50817b5760d35f8d940440c81858bba72c0c40da
fadi <fady.yosry2018@gmail.com>
2023-05-20 00:47:30 +0300

    remove unnecessary endponits

--- machineLearning/urls.py
+++ machineLearning/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import capture_frames,face_detection, classifyPose ,classify_pose_real_time , stream_camera, pose_classifier, classify_pose_real_time2
+from .views import capture_frames,face_detection, classifyPose ,stream_camera, pose_classifier
 
 urlpatterns = [
     path('stream_camera/', stream_camera, name='stream_camera'),
     path('face_detection/', face_detection, name='face_detection'),
     path('classify_pose/', classifyPose, name='classify_pose'),
-    path('classify_pose_real_time/', classify_pose_real_time, name='classify_pose_real_time'),
-    path('classify_pose_real_time2/', classify_pose_real_time2, name='classify_pose_real_time2'),
     path('capture_frames/', capture_frames, name='capture_frames'),
     path('pose_classifier/', pose_classifier, name='pose_classifier'),
 ]
