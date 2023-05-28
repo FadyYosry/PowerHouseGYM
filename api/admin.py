673da8abce4ed4ba3d5b8b93707a7186d382b649
fadi <fady.yosry2018@gmail.com>
2023-05-28 12:17:57 +0300

    add admin table

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Gym,Member,AdminGym
+from .models import Gym,Member,AdminGym, Admin
 # Register your models here.
 
 admin.site.register(Gym)
 admin.site.register(Member)
 admin.site.register(AdminGym)
+admin.site.register(Admin)
